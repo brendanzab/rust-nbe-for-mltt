bb110971fafab9f7439c2ea565a99278f015d9b0
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2018-11-24 23:41:24 +1100

    Use mutable vector in desugaring

--- src/desugar.rs
+++ src/desugar.rs
@@ -1,5 +1,3 @@
-use im;
-
 use syntax::{concrete, core, DbIndex, Ident, IdentHint, UniverseLevel};
 
 pub enum DesugarError {
@@ -8,11 +6,16 @@ pub enum DesugarError {
 
 pub fn desugar<'a>(
     concrete_term: &'a concrete::Term,
-    env: &im::Vector<Option<&'a Ident>>,
+    env: &mut Vec<Option<&'a Ident>>,
 ) -> Result<core::RcTerm, DesugarError> {
     match *concrete_term {
         concrete::Term::Var(ref ident) => {
-            match env.iter().enumerate().find(|(_, i)| **i == Some(ident)) {
+            match env
+                .iter()
+                .rev()
+                .enumerate()
+                .find(|(_, i)| **i == Some(ident))
+            {
                 None => Err(DesugarError::UnboundVar(ident.clone())),
                 Some((index, _)) => Ok(core::RcTerm::from(core::Term::Var(
                     IdentHint(Some(ident.clone())),
@@ -20,13 +23,18 @@ pub fn desugar<'a>(
                 ))),
             }
         },
-        concrete::Term::Let(ref ident, ref def, ref body) => Ok(core::RcTerm::from(
-            core::Term::Let(IdentHint(Some(ident.clone())), desugar(def, env)?, {
-                let mut env = env.clone();
-                env.push_front(Some(ident));
-                desugar(body, &env)?
-            }),
-        )),
+        concrete::Term::Let(ref ident, ref def, ref body) => {
+            let def = desugar(def, env)?;
+            env.push(Some(ident));
+            let body = desugar(body, env)?;
+            env.pop();
+
+            Ok(core::RcTerm::from(core::Term::Let(
+                IdentHint(Some(ident.clone())),
+                def,
+                body,
+            )))
+        },
         concrete::Term::Check(ref term, ref ann) => Ok(core::RcTerm::from(core::Term::Check(
             desugar(term, env)?,
             desugar(ann, env)?,
@@ -34,20 +42,28 @@ pub fn desugar<'a>(
         concrete::Term::Parens(ref term) => desugar(term, env),
 
         // Functions
-        concrete::Term::FunType(ref ident, ref param_ty, ref body_ty) => Ok(core::RcTerm::from(
-            core::Term::FunType(IdentHint(ident.clone()), desugar(param_ty, env)?, {
-                let mut env = env.clone();
-                env.push_front(ident.as_ref());
-                desugar(body_ty, &env)?
-            }),
-        )),
-        concrete::Term::FunIntro(ref ident, ref body) => Ok(core::RcTerm::from(
-            core::Term::FunIntro(IdentHint(Some(ident.clone())), {
-                let mut env = env.clone();
-                env.push_front(Some(ident));
-                desugar(body, &env)?
-            }),
-        )),
+        concrete::Term::FunType(ref ident, ref param_ty, ref body_ty) => {
+            let param_ty = desugar(param_ty, env)?;
+            env.push(ident.as_ref());
+            let body_ty = desugar(body_ty, env)?;
+            env.pop();
+
+            Ok(core::RcTerm::from(core::Term::FunType(
+                IdentHint(ident.clone()),
+                param_ty,
+                body_ty,
+            )))
+        },
+        concrete::Term::FunIntro(ref ident, ref body) => {
+            env.push(Some(ident));
+            let body = desugar(body, env)?;
+            env.pop();
+
+            Ok(core::RcTerm::from(core::Term::FunIntro(
+                IdentHint(Some(ident.clone())),
+                body,
+            )))
+        },
         concrete::Term::FunApp(ref fun, ref args) => {
             args.iter().fold(desugar(fun, env), |acc, arg| {
                 Ok(core::RcTerm::from(core::Term::FunApp(
@@ -58,13 +74,18 @@ pub fn desugar<'a>(
         },
 
         // Pairs
-        concrete::Term::PairType(ref ident, ref fst_ty, ref snd_ty) => Ok(core::RcTerm::from(
-            core::Term::PairType(IdentHint(ident.clone()), desugar(fst_ty, env)?, {
-                let mut env = env.clone();
-                env.push_front(ident.as_ref());
-                desugar(snd_ty, &env)?
-            }),
-        )),
+        concrete::Term::PairType(ref ident, ref fst_ty, ref snd_ty) => {
+            let fst_ty = desugar(fst_ty, env)?;
+            env.push(ident.as_ref());
+            let snd_ty = desugar(snd_ty, env)?;
+            env.pop();
+
+            Ok(core::RcTerm::from(core::Term::PairType(
+                IdentHint(ident.clone()),
+                fst_ty,
+                snd_ty,
+            )))
+        },
         concrete::Term::PairIntro(ref fst, ref snd) => Ok(core::RcTerm::from(
             core::Term::PairIntro(desugar(fst, env)?, desugar(snd, env)?),
         )),
