ca0eec369e572ac93d24508df30f6c4faf989bec
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2019-01-11 16:27:26 +1100

    Add commented out lines in test harness

--- crates/mltt-test-harness/src/lib.rs
+++ crates/mltt-test-harness/src/lib.rs
@@ -1,4 +1,8 @@
+// use mltt_concrete::desugar;
+// use mltt_concrete::elaborate;
 use mltt_concrete::lexer::Lexer;
+// use mltt_concrete::parser;
+// use mltt_core::validate;
 use std::fs;
 use std::path::Path;
 
@@ -9,7 +13,15 @@ pub fn run(_test_name: &str, test_path: impl AsRef<Path>) {
 
     Lexer::new(&src).for_each(|token| println!("{}", token.unwrap().1));
 
-    // TODO: parse
-    // TODO: elaborate
-    // TODO: validate
+    // TODO:
+
+    // let (concrete_module, errors) = parser::parse_module(Lexer::new(&src));
+    // assert_eq!(errors, vec![]);
+
+    // let raw_module = desugar::desugar_module(&raw_module);
+    // let (core_module, errors) = elaborate::elaborate_module(&raw_module);
+    // assert_eq!(errors, vec![]);
+
+    // let result = validate::validate_module(&core_module);
+    // assert_eq!(result, Ok(()));
 }
