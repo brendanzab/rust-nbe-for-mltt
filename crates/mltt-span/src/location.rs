5b0059a909b4191ec434f124262d9f79cf6d0649
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2019-01-30 13:58:07 +1100

    Add doc comments to location struct

--- crates/mltt-span/src/location.rs
+++ crates/mltt-span/src/location.rs
@@ -1,9 +1,13 @@
 use crate::{ByteIndex, ColumnIndex, LineIndex};
 
+/// A location in a source file.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Location {
+    /// The line index in the source file.
     pub line: LineIndex,
+    /// The column index in the source file.
     pub column: ColumnIndex,
+    /// The byte index in the source file.
     pub byte: ByteIndex,
 }
 
