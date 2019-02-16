a98bc34975b08ecfce8bd8b25231ff9c15643eea
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2019-02-16 14:42:57 +1100

    Clean up pretty printing of concrete syntax

--- crates/mltt-concrete/src/syntax.rs
+++ crates/mltt-concrete/src/syntax.rs
@@ -118,6 +118,19 @@ pub struct RecordTypeField {
     pub ann: Term,
 }
 
+impl RecordTypeField {
+    /// Convert the field into a pretty-printable document
+    pub fn to_doc(&self) -> Doc<'_, BoxDoc<'_, ()>> {
+        Doc::nil()
+            .append(&self.label)
+            .append(Doc::space())
+            .append(":")
+            .append(Doc::space())
+            .append(self.ann.to_doc())
+            .append(";")
+    }
+}
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum RecordIntroField {
     Punned {
@@ -129,6 +142,41 @@ pub enum RecordIntroField {
         term_ty: Option<Term>,
         term: Term,
     },
+}
+
+impl RecordIntroField {
+    /// Convert the field into a pretty-printable document
+    pub fn to_doc(&self) -> Doc<'_, BoxDoc<'_, ()>> {
+        match self {
+            RecordIntroField::Punned { label } => Doc::text(label).append(";"),
+            RecordIntroField::Explicit {
+                label,
+                patterns,
+                term_ty,
+                term,
+            } => {
+                let patterns = Doc::intersperse(patterns.iter().map(Pattern::to_doc), Doc::space());
+                let term_ty = term_ty.as_ref().map_or(Doc::nil(), |term_ty| {
+                    Doc::nil()
+                        .append(":")
+                        .append(Doc::space())
+                        .append(term_ty.to_doc())
+                        .append(Doc::space())
+                });
+
+                Doc::nil()
+                    .append(label)
+                    .append(Doc::space())
+                    .append(patterns)
+                    .append(Doc::space())
+                    .append(term_ty)
+                    .append("=")
+                    .append(Doc::space())
+                    .append(term.to_doc())
+                    .append(";")
+            },
+        }
+    }
 }
 
 /// Concrete terms
@@ -172,180 +220,114 @@ pub enum Term {
 impl Term {
     /// Convert the term into a pretty-printable document
     pub fn to_doc(&self) -> Doc<'_, BoxDoc<'_, ()>> {
-        // Using precedence climbing (mirroring the language grammar) in
-        // order to cut down on extraneous parentheses.
-
-        fn to_doc_term(term: &Term) -> Doc<'_, BoxDoc<'_, ()>> {
-            match term {
-                Term::Ann(term, ann) => Doc::nil()
-                    .append(to_doc_expr(term))
-                    .append(Doc::space())
-                    .append(":")
-                    .append(Doc::space())
-                    .append(to_doc_app(ann)),
-                _ => to_doc_expr(term),
-            }
-        }
-
-        fn to_doc_expr(term: &Term) -> Doc<'_, BoxDoc<'_, ()>> {
-            match term {
-                Term::Let(def_name, def, body) => Doc::nil()
-                    .append("let")
-                    .append(Doc::space())
-                    .append(def_name)
-                    .append(Doc::space())
-                    .append("=")
-                    .append(Doc::space())
-                    .append(to_doc_app(def))
-                    .append(Doc::space())
-                    .append("in")
-                    .append(Doc::space())
-                    .append(to_doc_term(body)),
-                Term::FunType(params, body_ty) => Doc::nil()
-                    .append(Doc::group(
+        match self {
+            Term::Ann(term, ann) => Doc::nil()
+                .append(term.to_doc())
+                .append(Doc::space())
+                .append(":")
+                .append(Doc::space())
+                .append(ann.to_doc()),
+            Term::Let(def_name, def, body) => Doc::nil()
+                .append("let")
+                .append(Doc::space())
+                .append(def_name)
+                .append(Doc::space())
+                .append("=")
+                .append(Doc::space())
+                .append(def.to_doc())
+                .append(Doc::space())
+                .append("in")
+                .append(Doc::space())
+                .append(body.to_doc()),
+            Term::FunType(params, body_ty) => {
+                let params = Doc::intersperse(
+                    params.iter().map(|(param_names, param_ty)| {
                         Doc::nil()
-                            .append("Fun")
-                            .append(Doc::space())
+                            .append("(")
                             .append(Doc::intersperse(
-                                params.iter().map(|(param_names, param_ty)| {
-                                    Doc::nil()
-                                        .append("(")
-                                        .append(Doc::intersperse(
-                                            param_names.iter().map(Doc::text),
-                                            Doc::space(),
-                                        ))
-                                        .append(Doc::space())
-                                        .append(":")
-                                        .append(Doc::space())
-                                        .append(to_doc_term(param_ty))
-                                        .append(")")
-                                }),
+                                param_names.iter().map(Doc::text),
                                 Doc::space(),
-                            )),
-                    ))
+                            ))
+                            .append(Doc::space())
+                            .append(":")
+                            .append(Doc::space())
+                            .append(param_ty.to_doc())
+                            .append(")")
+                    }),
+                    Doc::space(),
+                );
+
+                Doc::nil()
+                    .append(Doc::text("Fun").append(Doc::space()).append(params))
                     .append(Doc::space())
                     .append("->")
                     .append(Doc::space())
-                    .append(to_doc_app(body_ty)),
-                Term::FunIntro(param_names, body) => Doc::nil()
-                    .append("fun")
-                    .append(Doc::space())
-                    .append(Doc::intersperse(
-                        param_names.iter().map(Pattern::to_doc),
-                        Doc::space(),
-                    ))
-                    .append(Doc::space())
-                    .append("=>")
-                    .append(Doc::space())
-                    .append(to_doc_app(body)),
-                Term::RecordType(ty_fields) if ty_fields.is_empty() => Doc::text("Record {}"),
-                Term::RecordType(ty_fields) => Doc::nil()
+                    .append(body_ty.to_doc())
+            },
+            Term::FunIntro(param_names, body) => Doc::nil()
+                .append("fun")
+                .append(Doc::space())
+                .append(Doc::intersperse(
+                    param_names.iter().map(Pattern::to_doc),
+                    Doc::space(),
+                ))
+                .append(Doc::space())
+                .append("=>")
+                .append(Doc::space())
+                .append(body.to_doc()),
+            Term::RecordType(ty_fields) if ty_fields.is_empty() => Doc::text("Record {}"),
+            Term::RecordType(ty_fields) => {
+                let ty_fields = Doc::intersperse(
+                    ty_fields.iter().map(RecordTypeField::to_doc),
+                    Doc::newline(),
+                );
+
+                Doc::nil()
                     .append("Record")
                     .append(Doc::space())
                     .append("{")
                     .append(Doc::newline())
-                    .append(
-                        Doc::intersperse(
-                            ty_fields.iter().map(|ty_field| {
-                                Doc::nil()
-                                    .append(&ty_field.label)
-                                    .append(Doc::space())
-                                    .append(":")
-                                    .append(Doc::space())
-                                    .append(to_doc_app(&ty_field.ann))
-                                    .append(";")
-                            }),
-                            Doc::newline(),
-                        )
-                        .nest(4),
-                    )
+                    .append(ty_fields.nest(4))
                     .append(Doc::newline())
-                    .append("}"),
-                Term::RecordIntro(intro_fields) if intro_fields.is_empty() => {
-                    Doc::text("record {}")
-                },
-                Term::RecordIntro(intro_fields) => Doc::nil()
+                    .append("}")
+            },
+            Term::RecordIntro(intro_fields) if intro_fields.is_empty() => Doc::text("record {}"),
+            Term::RecordIntro(intro_fields) => {
+                let intro_fields = Doc::intersperse(
+                    intro_fields.iter().map(RecordIntroField::to_doc),
+                    Doc::newline(),
+                );
+
+                Doc::nil()
                     .append("record")
                     .append(Doc::space())
                     .append("{")
                     .append(Doc::newline())
-                    .append(
-                        Doc::intersperse(
-                            intro_fields.iter().map(|intro_field| match intro_field {
-                                RecordIntroField::Punned { label } => Doc::text(label).append(";"),
-                                RecordIntroField::Explicit {
-                                    label,
-                                    patterns,
-                                    term_ty,
-                                    term,
-                                } => Doc::nil()
-                                    .append(label)
-                                    .append(Doc::space())
-                                    .append(Doc::intersperse(
-                                        patterns.iter().map(Pattern::to_doc),
-                                        Doc::space(),
-                                    ))
-                                    .append(Doc::space())
-                                    .append(term_ty.as_ref().map_or(Doc::nil(), |term_ty| {
-                                        Doc::nil()
-                                            .append(":")
-                                            .append(Doc::space())
-                                            .append(to_doc_app(term_ty))
-                                            .append(Doc::space())
-                                    }))
-                                    .append("=")
-                                    .append(Doc::space())
-                                    .append(to_doc_app(term))
-                                    .append(";"),
-                            }),
-                            Doc::newline(),
-                        )
-                        .nest(4),
-                    )
+                    .append(intro_fields.nest(4))
                     .append(Doc::newline())
-                    .append("}"),
-                _ => to_doc_arrow(term),
-            }
-        }
+                    .append("}")
+            },
+            Term::FunArrowType(param_ty, body_ty) => Doc::nil()
+                .append(param_ty.to_doc())
+                .append(Doc::space())
+                .append("->")
+                .append(Doc::space())
+                .append(body_ty.to_doc()),
+            Term::FunApp(fun, args) => {
+                let args = Doc::intersperse(args.iter().map(Term::to_doc), Doc::space());
 
-        fn to_doc_arrow(term: &Term) -> Doc<'_, BoxDoc<'_, ()>> {
-            match term {
-                Term::FunArrowType(param_ty, body_ty) => Doc::nil()
-                    .append(to_doc_app(param_ty))
+                Doc::nil()
+                    .append(fun.to_doc())
                     .append(Doc::space())
-                    .append("->")
-                    .append(Doc::space())
-                    .append(to_doc_app(body_ty)),
-                _ => to_doc_app(term),
-            }
+                    .append(args)
+            },
+            Term::Var(name) => Doc::as_string(name),
+            Term::Literal(literal) => literal.to_doc(),
+            Term::Parens(term) => Doc::text("(").append(term.to_doc()).append(")"),
+            Term::RecordProj(record, label) => record.to_doc().append(".").append(label),
+            Term::Universe(None) => Doc::text("Type"),
+            Term::Universe(Some(level)) => Doc::text("Type^").append(Doc::as_string(level)),
         }
-
-        fn to_doc_app(term: &Term) -> Doc<'_, BoxDoc<'_, ()>> {
-            match term {
-                Term::FunApp(fun, args) => Doc::nil()
-                    .append(to_doc_term(fun))
-                    .append(Doc::space())
-                    .append(Doc::intersperse(
-                        args.iter().map(to_doc_atomic),
-                        Doc::space(),
-                    )),
-                _ => to_doc_atomic(term),
-            }
-        }
-
-        fn to_doc_atomic(term: &Term) -> Doc<'_, BoxDoc<'_, ()>> {
-            match term {
-                Term::Var(name) => Doc::as_string(name),
-                Term::Literal(literal) => literal.to_doc(),
-                Term::Parens(term) => Doc::text("(").append(to_doc_term(term)).append(")"),
-                Term::RecordProj(record, label) => to_doc_atomic(record).append(".").append(label),
-                Term::Universe(None) => Doc::text("Type"),
-                Term::Universe(Some(level)) => Doc::text("Type^").append(Doc::as_string(level)),
-                _ => Doc::text("(").append(to_doc_term(term)).append(")"),
-            }
-        }
-
-        to_doc_term(self)
     }
 }
 
