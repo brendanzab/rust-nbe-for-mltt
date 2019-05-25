41340e772c6ddbe1e5d98a85c223b97e315cb830
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2019-05-25 14:52:09 +1000

    Pull out test setup into function

--- crates/mltt-test/src/support.rs
+++ crates/mltt-test/src/support.rs
@@ -9,6 +9,13 @@ use std::fs;
 const TESTS_DIR: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/../../tests");
 const REPORTING_CONFIG: language_reporting::DefaultConfig = language_reporting::DefaultConfig;
 
+fn setup() -> (Files, mltt_core::meta::Env, mltt_elaborate::Context) {
+    let files = Files::new();
+    let metas = mltt_core::meta::Env::new();
+    let context = mltt_elaborate::Context::default();
+    (files, metas, context)
+}
+
 fn load_file(files: &mut Files, path: String) -> FileId {
     let src = fs::read_to_string(&path).unwrap_or_else(|error| panic!("{}", error));
     files.add(path, src)
@@ -18,10 +25,7 @@ pub fn run_sample(name: &str) {
     let _ = pretty_env_logger::try_init();
     let writer = StandardStream::stdout(ColorChoice::Always);
 
-    let mut files = Files::new();
-    let mut metas = mltt_core::meta::Env::new();
-    let context = mltt_elaborate::Context::default();
-    let validation_context = context.validation_context();
+    let (mut files, mut metas, context) = setup();
 
     let module_path = format!("{}/samples/{}.mltt", TESTS_DIR, name);
     let module_file_id = load_file(&mut files, module_path);
@@ -44,7 +48,7 @@ pub fn run_sample(name: &str) {
         )
     };
 
-    validate::check_module(&validation_context, &metas, &module)
+    validate::check_module(&context.validation_context(), &metas, &module)
         .unwrap_or_else(|error| panic!("{}", error));
 }
 
@@ -52,10 +56,7 @@ pub fn run_elaborate_check_pass(name: &str) {
     let _ = pretty_env_logger::try_init();
     let writer = StandardStream::stdout(ColorChoice::Always);
 
-    let mut files = Files::new();
-    let mut metas = mltt_core::meta::Env::new();
-    let context = mltt_elaborate::Context::default();
-    let validation_context = context.validation_context();
+    let (mut files, mut metas, context) = setup();
 
     let term_path = format!("{}/elaborate/check-pass/{}.term.mltt", TESTS_DIR, name);
     let ty_path = format!("{}/elaborate/check-pass/{}.type.mltt", TESTS_DIR, name);
@@ -80,7 +81,7 @@ pub fn run_elaborate_check_pass(name: &str) {
         )
     };
 
-    validate::synth_universe(&validation_context, &metas, &expected_ty)
+    validate::synth_universe(&context.validation_context(), &metas, &expected_ty)
         .unwrap_or_else(|error| panic!("{}", error));
 
     let expected_ty = nbe::eval_term(context.prims(), &metas, context.values(), &expected_ty)
@@ -103,7 +104,7 @@ pub fn run_elaborate_check_pass(name: &str) {
             })
     };
 
-    validate::check_term(&validation_context, &metas, &term, &expected_ty)
+    validate::check_term(&context.validation_context(), &metas, &term, &expected_ty)
         .unwrap_or_else(|error| panic!("{}", error));
 }
 
@@ -111,10 +112,7 @@ pub fn run_elaborate_synth_pass(name: &str) {
     let _ = pretty_env_logger::try_init();
     let writer = StandardStream::stdout(ColorChoice::Always);
 
-    let mut files = Files::new();
-    let mut metas = mltt_core::meta::Env::new();
-    let context = mltt_elaborate::Context::default();
-    let validation_context = context.validation_context();
+    let (mut files, mut metas, context) = setup();
 
     let term_path = format!("{}/elaborate/synth-pass/{}.term.mltt", TESTS_DIR, name);
     let ty_path = format!("{}/elaborate/synth-pass/{}.type.mltt", TESTS_DIR, name);
@@ -138,7 +136,7 @@ pub fn run_elaborate_synth_pass(name: &str) {
             })
     };
 
-    validate::synth_term(&validation_context, &metas, &term)
+    validate::synth_term(&context.validation_context(), &metas, &term)
         .unwrap_or_else(|error| panic!("{}", error));
 
     let (expected_ty, _level) = {
@@ -159,7 +157,7 @@ pub fn run_elaborate_synth_pass(name: &str) {
         )
     };
 
-    validate::synth_universe(&validation_context, &metas, &expected_ty)
+    validate::synth_universe(&context.validation_context(), &metas, &expected_ty)
         .unwrap_or_else(|error| panic!("{}", error));
 
     let expected_ty = nbe::eval_term(context.prims(), &metas, context.values(), &expected_ty)
