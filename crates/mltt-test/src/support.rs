c97c6116ec55124f4fd82492e1563b2c4db99bd8
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2019-06-02 22:48:27 +1000

    Add initial support for snapshot regeneration

--- crates/mltt-test/src/support.rs
+++ crates/mltt-test/src/support.rs
@@ -5,11 +5,19 @@ use mltt_elaborate::MetaInsertion;
 use mltt_parse::lexer::Lexer;
 use mltt_parse::parser;
 use mltt_span::{File, FileId, FileSpan, Files};
-use std::fs;
+use std::io::Write;
+use std::path::{Path, PathBuf};
 use std::rc::Rc;
+use std::{env, fs};
 
 const TESTS_DIR: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/../../tests");
 const REPORTING_CONFIG: language_reporting::DefaultConfig = language_reporting::DefaultConfig;
+
+macro_rules! path_buf {
+    ($fmt_str:literal, $($arg:tt),*) => {
+        PathBuf::from(format!($fmt_str, $($arg),*))
+    };
+}
 
 fn setup() -> (Files, mltt_core::meta::Env, mltt_elaborate::Context) {
     let files = Files::new();
@@ -18,9 +26,14 @@ fn setup() -> (Files, mltt_core::meta::Env, mltt_elaborate::Context) {
     (files, metas, context)
 }
 
-fn load_file(files: &mut Files, path: String) -> FileId {
-    let src = fs::read_to_string(&path).unwrap_or_else(|error| panic!("{}", error));
-    files.add(path, src)
+fn load_test_file(files: &mut Files, path: impl AsRef<Path>) -> Option<FileId> {
+    let path = path.as_ref();
+    if path.exists() {
+        let src = fs::read_to_string(&path).unwrap_or_else(|error| panic!("{}", error));
+        Some(files.add(path.display().to_string(), src))
+    } else {
+        None
+    }
 }
 
 fn emit_diagnostic<'a, T>(
@@ -70,12 +83,13 @@ fn check_term(
 }
 
 fn synth_term(
+    files: &Files,
     context: &mltt_elaborate::Context,
     metas: &mut mltt_core::meta::Env,
-    concrete_term_file: &File,
-    expected_ty: &Rc<domain::Type>,
+    concrete_term_file_id: FileId,
+    expected_ty_file_id: Option<FileId>,
 ) -> Result<(Rc<syntax::Term>, Rc<domain::Type>), Diagnostic<FileSpan>> {
-    let lexer = Lexer::new(&concrete_term_file);
+    let lexer = Lexer::new(&files[concrete_term_file_id]);
     let concrete_term = parser::parse_term(lexer)?;
     // FIXME: check lexer for errors
 
@@ -84,19 +98,23 @@ fn synth_term(
     validate::synth_term(&context.validation_context(), &metas, &term)
         .unwrap_or_else(|error| panic!("{}", error));
 
-    // Verify that we got the expected type (sans subtyping)
-    let prims = context.prims();
-    let size = context.values().size();
-    if !nbe::check_ty(&prims, &metas, size, false, &term_ty, &expected_ty)
-        .unwrap_or_else(|error| panic!("{}", error))
-    {
-        panic!("unequal types");
-    }
+    if let Some(expected_ty_file_id) = expected_ty_file_id.into() {
+        let expected_ty = synth_universe(&context, metas, &files[expected_ty_file_id])?;
 
-    // Ensure that the checking also works
-    let term2 = mltt_elaborate::check_term(context, metas, &concrete_term, expected_ty)?;
-    validate::check_term(&context.validation_context(), &metas, &term2, &expected_ty)
-        .unwrap_or_else(|error| panic!("{}", error));
+        // Verify that we got the expected type (sans subtyping)
+        let prims = context.prims();
+        let size = context.values().size();
+        if !nbe::check_ty(&prims, &metas, size, false, &term_ty, &expected_ty)
+            .unwrap_or_else(|error| panic!("{}", error))
+        {
+            panic!("unequal types");
+        }
+
+        // Ensure that the checking also works
+        let term2 = mltt_elaborate::check_term(context, metas, &concrete_term, &expected_ty)?;
+        validate::check_term(&context.validation_context(), &metas, &term2, &expected_ty)
+            .unwrap_or_else(|error| panic!("{}", error));
+    }
 
     Ok((term, term_ty))
 }
@@ -107,8 +125,9 @@ pub fn run_sample(name: &str) {
 
     let (mut files, mut metas, context) = setup();
 
-    let module_path = format!("{}/samples/{}.mltt", TESTS_DIR, name);
-    let module_file_id = load_file(&mut files, module_path);
+    let module_path = path_buf!("{}/samples/{}.mltt", TESTS_DIR, name);
+    let module_file_id = load_test_file(&mut files, &module_path)
+        .unwrap_or_else(|| panic!("failed to find file: `{}`", module_path.display()));
 
     let lexer = Lexer::new(&files[module_file_id]);
     let concrete_module =
@@ -127,10 +146,12 @@ pub fn run_elaborate_check_pass(name: &str) {
 
     let (mut files, mut metas, context) = setup();
 
-    let term_path = format!("{}/elaborate/check-pass/{}.term.mltt", TESTS_DIR, name);
-    let ty_path = format!("{}/elaborate/check-pass/{}.type.mltt", TESTS_DIR, name);
-    let term_file_id = load_file(&mut files, term_path);
-    let ty_file_id = load_file(&mut files, ty_path);
+    let term_path = path_buf!("{}/elaborate/check-pass/{}.term.mltt", TESTS_DIR, name);
+    let ty_path = path_buf!("{}/elaborate/check-pass/{}.type.mltt", TESTS_DIR, name);
+    let term_file_id = load_test_file(&mut files, &term_path)
+        .unwrap_or_else(|| panic!("failed to find file: `{}`", term_path.display()));
+    let ty_file_id = load_test_file(&mut files, &ty_path)
+        .unwrap_or_else(|| panic!("failed to find file: `{}`", ty_path.display()));
 
     let expected_ty = synth_universe(&context, &mut metas, &files[ty_file_id])
         .unwrap_or_else(emit_diagnostic(&writer, &files));
@@ -144,10 +165,12 @@ pub fn run_elaborate_check_fail(name: &str) {
 
     let (mut files, mut metas, context) = setup();
 
-    let term_path = format!("{}/elaborate/check-fail/{}.term.mltt", TESTS_DIR, name);
-    let ty_path = format!("{}/elaborate/check-fail/{}.type.mltt", TESTS_DIR, name);
-    let term_file_id = load_file(&mut files, term_path);
-    let ty_file_id = load_file(&mut files, ty_path);
+    let term_path = path_buf!("{}/elaborate/check-fail/{}.term.mltt", TESTS_DIR, name);
+    let ty_path = path_buf!("{}/elaborate/check-fail/{}.type.mltt", TESTS_DIR, name);
+    let term_file_id = load_test_file(&mut files, &term_path)
+        .unwrap_or_else(|| panic!("failed to find file: `{}`", term_path.display()));
+    let ty_file_id = load_test_file(&mut files, &ty_path)
+        .unwrap_or_else(|| panic!("failed to find file: `{}`", ty_path.display()));
 
     let lexer = Lexer::new(&files[term_file_id]);
     let _concrete_term = parser::parse_term(lexer).unwrap_or_else(emit_diagnostic(&writer, &files));
@@ -163,15 +186,24 @@ pub fn run_elaborate_synth_pass(name: &str) {
 
     let (mut files, mut metas, context) = setup();
 
-    let term_path = format!("{}/elaborate/synth-pass/{}.term.mltt", TESTS_DIR, name);
-    let ty_path = format!("{}/elaborate/synth-pass/{}.type.mltt", TESTS_DIR, name);
-    let term_file_id = load_file(&mut files, term_path);
-    let ty_file_id = load_file(&mut files, ty_path);
+    let term_path = path_buf!("{}/elaborate/synth-pass/{}.term.mltt", TESTS_DIR, name);
+    let ty_path = path_buf!("{}/elaborate/synth-pass/{}.type.mltt", TESTS_DIR, name);
+    let term_file_id = load_test_file(&mut files, &term_path)
+        .unwrap_or_else(|| panic!("failed to find file: `{}`", term_path.display()));
+    let ty_file_id = load_test_file(&mut files, &ty_path);
 
-    let expected_ty = synth_universe(&context, &mut metas, &files[ty_file_id])
+    let (_, synth_ty) = synth_term(&files, &context, &mut metas, term_file_id, ty_file_id)
         .unwrap_or_else(emit_diagnostic(&writer, &files));
-    synth_term(&context, &mut metas, &files[term_file_id], &expected_ty)
-        .unwrap_or_else(emit_diagnostic(&writer, &files));
+
+    if ty_file_id.is_none() {
+        if env::var("MLTT_BLESS").is_ok() {
+            let mut ty_file = fs::File::create(ty_path).unwrap();
+            let synth_ty = context.value_to_doc(&metas, &synth_ty);
+            writeln!(ty_file, "{}", synth_ty.pretty(80)).unwrap();
+        } else {
+            panic!("no snapshot found for {} - rerun with `MLTT_BLESS=1`", name);
+        }
+    }
 }
 
 pub fn run_elaborate_synth_fail(name: &str) {
@@ -180,8 +212,9 @@ pub fn run_elaborate_synth_fail(name: &str) {
 
     let (mut files, mut _metas, _context) = setup();
 
-    let term_path = format!("{}/elaborate/synth-fail/{}.term.mltt", TESTS_DIR, name);
-    let term_file_id = load_file(&mut files, term_path);
+    let term_path = path_buf!("{}/elaborate/synth-fail/{}.term.mltt", TESTS_DIR, name);
+    let term_file_id = load_test_file(&mut files, &term_path)
+        .unwrap_or_else(|| panic!("failed to find file: `{}`", term_path.display()));
 
     let lexer = Lexer::new(&files[term_file_id]);
     let _concrete_term = parser::parse_term(lexer).unwrap_or_else(emit_diagnostic(&writer, &files));
